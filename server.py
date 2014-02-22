f785a5f80ec42e5806b9226b807f698992400a03
Kang Suh <suhkang@arctic.cse.msu.edu>
2014-02-22 16:34:43 -0500

    rollback

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 import random
 import socket
 import time
-<<<<<<< HEAD
 from StringIO import StringIO
 from urlparse import urlparse, parse_qs
 from app import make_app
@@ -98,27 +97,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-=======
-
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
-
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
-
-s.listen(5)                 # Now wait for client connection.
-
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    print    
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send('Content-Type: text/html\r\n\r\n')
-    c.send("<h1>Hello, world</h1> this is suhkang's Web server.")
-    c.close()
-
->>>>>>> master
