7149c894aaa92f5fa97a4e94cd4a03bf945d14b3
Kang Suh <suhkang@arctic.cse.msu.edu>
2014-04-03 11:31:38 -0400

    added url handler

--- imageapp/__init__.py
+++ imageapp/__init__.py
@@ -5,7 +5,7 @@ from quixote.publish import Publisher
 # this imports the class RootDirectory from the file 'root.py'
 from .root import RootDirectory
 from . import html, image
-import os
+#import os
 
 def create_publisher():
      p = Publisher(RootDirectory(), display_exceptions='plain')
@@ -15,11 +15,13 @@ def create_publisher():
 
 def setup():                            # stuff that should be run once.
     html.init_templates()
-    
-    for file in os.listdir('imageapp'):
-        if file.endswith(".png"):
-            img_data = open('imageapp/'+file).read()
-            image.add_image(img_data)
+
+    img_data = open('imageapp/tux.png', 'rb').read()
+    image.add_image('imageapp/tux.png', img_data)
+    # for file in os.listdir('imageapp'):
+    #     if file.endswith(".png"):
+    #         img_data = open('imageapp/'+file).read()
+    #         image.add_image(img_data)
     
 
 def teardown():                         # stuff that should be run once.
